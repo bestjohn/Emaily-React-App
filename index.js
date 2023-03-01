af24cb3fb4f1178526a7b69d960c80c63ca5ff8c
Ben St <benst@Bens-MacBook-Pro.local>
2023-03-01 14:08:20 -0500

    refactored code

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
+require('./services/passport');
 const app = express();
 
-app.get('/',(req,res) => {
-    res.send({hi: 'boo'});
-})
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
