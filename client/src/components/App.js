cbce8beca3b4c818c00c257ed8d760dbfdce0ae8
Ben St <benst@Bens-MacBook-Pro.local>
2023-03-26 20:56:30 -0400

    add reducers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Header from './Header';
+
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
     return (
         <div>
-            Hi There!
+            <BrowserRouter>
+            <div>
+                <Header />
+                <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/surveys" element={<Dashboard />} />
+                <Route path="/surveys/new" element={<SurveyNew />} />
+                </Routes>
+            </div>
+            </BrowserRouter>
         </div>
     );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,23 @@
+import materializeCSS from 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
+import {Provider} from 'react-redux';
+import {legacy_createStore as createStore,applyMiddleware} from 'redux';//recommended to use redux-toolkit configureStore in production
 
 import App from './components/App';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const store = createStore( reducers, {},applyMiddleware());
+
+
+
+//provider reads from store and notifies all child components of new state updates...ithink
+/*
+ReactDOM.render(
+<Provider store={store}><App /></Provider>, 
+document.querySelector('#root')
+);
+*/
+
+const root = ReactDOMClient.createRoot(document.querySelector('#root'));
+root.render(<Provider store={store}><App /></Provider>);
